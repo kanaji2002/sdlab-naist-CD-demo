daceca81f611a868394cc6b1beaea281cc91e5cf
kanaji2002 <rinntarou2002520@gmail.com>
2025-12-27 10:20:17 +0900

    add

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
          </tr>
          <tr>
            <td>金地 琳太郎</td>
-           <td>2024/3/27
+           <td>2024/3/2
          </tr>
        </table>      
 </main>
